1cb6dcf8cfebd2a553cddb6ac43e27e27b855df4
Nick Palmer <nick@sluggardy.net>
2012-04-23 22:03:24 +0200

    Add hidden fields needed to keep things working.

--- src/interdroid/vdb/avro/AvroSchema.java
+++ src/interdroid/vdb/avro/AvroSchema.java
@@ -1,3 +1,33 @@
+/*
+ * Copyright (c) 2008-2012 Vrije Universiteit, The Netherlands All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Vrije Universiteit nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package interdroid.vdb.avro;
 
 
@@ -88,8 +118,8 @@ public final class AvroSchema {
 			+ "\n          \"fields\": ["
 			+ "\n              {\"name\": \"name\", \"type\": \"string\", \"ui.label\": \"Type Name\", \"ui.required\" : \"true\", \"ui.list\": \"true\"},"
 //			+ "\n              {\"name\": \"doc\", \"type\": \"string\"},"
-//			+ "\n              {\"name\": \"namespace\", \"type\": \"string\", \"ui.list\": \"true\", \"ui.label\": \"Repository Name\"},"
-//			+ "\n              {\"name\": \"aliases\", \"type\": {\"type\": \"array\", \"items\": {\"type\": \"string\"}}},"
+			+ "\n              {\"name\": \"namespace\", \"type\": \"string\", \"ui.list\": \"true\", \"ui.label\": \"Repository Name\", \"ui.visible\": \"false\"},"
+			+ "\n              {\"name\": \"aliases\", \"type\": {\"type\": \"array\", \"items\": {\"type\": \"string\"}}, \"ui.visible\": \"false\"},"
 			+ "\n              {\"name\": \"fields\","
 			+ "\n               \"type\": {\"type\": \"array\", \"items\":"
 			+ "\n                        {\"type\": \"record\", \"name\": \"FieldDef\","
@@ -98,7 +128,7 @@ public final class AvroSchema {
 //			+ "\n                             {\"name\": \"label\", \"type\": \"string\"},"
 //			+ "\n                             {\"name\": \"doc\", \"type\": \"string\"},"
 			+ "\n                             {\"name\": \"list\", \"ui.label\": \"Show In List\", \"type\": \"boolean\", \"default\": true},"
-//			+ "\n                             {\"name\": \"aliases\", \"type\": {\"type\": \"array\", \"items\": {\"type\": \"string\"}}},"
+			+ "\n                             {\"name\": \"aliases\", \"type\": {\"type\": \"array\", \"items\": {\"type\": \"string\"}}, \"ui.visible\":\"false\"},"
 			+ "\n                             {\"name\": \"type\", \"default\": \"String\", \"type\":"
 			+ "\n                                 {\"type\": \"enum\", \"name\": \"SimpleType\","
 			+ "\n                                  \"ui.label\": \"Type\", \"symbols\": ["
